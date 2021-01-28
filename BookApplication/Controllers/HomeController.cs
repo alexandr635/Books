dd4917742c628a4c49eff9e770313830e738a324
aleksandr635 <alexandr2002vaganov@gmail.com>
2021-01-28 15:34:40 +0300

    full functionality

--- BookApplication/Controllers/HomeController.cs
+++ BookApplication/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
 ﻿using Data;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
 {
@@ -22,13 +19,54 @@ namespace WebAPI.Controllers
             return View(db.Books.ToList());
         }
 
+        [Route("Home/Book/{Id?}")]
         [HttpGet]
         public IActionResult Book(int? id)
         {
             if (id == null)
                 return RedirectToAction("Index");
-            ViewBag.Id = id;
+            ViewBag.BookId = id;
             return View(db.Books.FirstOrDefault(b => b.Id == id));
         }
+
+        [Route("AddBook")]
+        [HttpGet]
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+
+        [Route("AddBook")]
+        [HttpPost]
+        public string AddBook(Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+            return "Книга добавлена";
+        }
+
+
+        [Route("Home/Change/{Id?}")]
+        [HttpGet]
+        public IActionResult Change(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("Index");
+            ViewBag.BookId = id;
+            return View(db.Books.FirstOrDefault(b => b.Id == id));
+        }
+
+        [Route("Home/Change/{Id?}")]
+        [HttpPost]
+        public IActionResult Change(Book book)
+        {
+            Book currentBook = db.Books.FirstOrDefault(b => b.Id == book.Id);
+            currentBook.Title = book.Title;
+            currentBook.DescriptionLong = book.DescriptionLong;
+            currentBook.DescriptionShort = book.DescriptionShort;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
